30d88de99678375d99511be8fb63182862d02be3
deotamafrwddeotamapaul-debug <deotama.frwd.deotama.paul@gmail.com>
2026-02-07 03:54:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("JS is running ✅");
+
+const stem = document.querySelector(".stem");
+const msg = document.getElementById("msg");
+
+stem.style.height = "120px";
+stem.style.background = "darkgreen";
+
+msg.textContent = "The plant grew 🌿";
